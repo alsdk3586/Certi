a5231ee9791198bc644f258978810e230c6f8515
likelionSungGuk <sung9gi@gmail.com>
2021-04-28 16:50:15 +0900

    feat: 🎸 create page navigation skeleton
    ✅ Closes: #S04P31A407-141

---
+++ frontend/src/Components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Router from './Router';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Router />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/Components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default () => (
+  <header>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/calendar">Calendar</a></li>
+      <li><a href="/board">Board</a></li>
+      <li><a href="/certi">Certi</a></li>
+      <li><a href="/user">User</a></li>
+    </ul>
+  </header>
+)
