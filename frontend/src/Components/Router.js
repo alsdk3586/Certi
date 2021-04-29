5a52cda737b00af3f00aea05fc40ee9a8f2f2f63
likelionSungGuk <sung9gi@gmail.com>
2021-04-29 15:58:40 +0900

    feat: 🎸 create navbar design
    ✅ Closes: #S04P31A407-142

--- frontend/src/Components/Router.js
+++ frontend/src/Components/Router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
+import Navbar from '../Components/Navbar';
 import Home from '../Routes/Home';
 import Calendar from '../Routes/Calendar';
 import Board from '../Routes/Board';
@@ -8,13 +9,16 @@ import User from '../Routes/User';
 
 export default () => (
   <Router>
-    <Switch>
-      <Route path="/" exact component = {Home} />
-      <Route path="/calendar" exact component = {Calendar} />
-      <Route path="/board" exact component = {Board} />
-      <Route path="/certi" exact component = {Certi} />
-      <Route path="/user" exact component = {User} />
-      <Redirect from="*" to="/" />
-    </Switch>
+    <>
+      <Navbar /> 
+      <Switch>
+        <Route path="/" exact component = {Home} />
+        <Route path="/calendar" exact component = {Calendar} />
+        <Route path="/board" exact component = {Board} />
+        <Route path="/certi" exact component = {Certi} />
+        <Route path="/user" exact component = {User} />
+        <Redirect from="*" to="/" />
+      </Switch>
+    </>
   </Router>
 );
