fb27ca4a423c0bc753d91d827b97cc807f298628
김혜민 <alsdk3586@hanmail.net>
2021-04-30 16:10:40 +0900

    chore: add LikeRespository, LikeController

---
+++ backend/src/main/java/com/ssafy/certi/repository/LikesRepository.java
@@ -0,0 +1,8 @@
+package com.ssafy.certi.repository;
+
+import com.ssafy.certi.domain.Likes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LikesRepository extends JpaRepository<Likes, Long> {
+
+}
