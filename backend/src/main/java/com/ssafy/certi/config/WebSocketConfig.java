ff1529cd4a2f9267905781f699a19dbaa1dce5d7
KSJ <khb3221@naver.com>
2021-05-03 10:00:29 +0900

    fix: 소켓 설정 수정

--- backend/src/main/java/com/ssafy/certi/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/certi/config/WebSocketConfig.java
@@ -1,21 +1,29 @@
 package com.ssafy.certi.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer{
 
     //웹소켓 엔드포인트 정의 ws://localhost:8080/ws
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
         registry.addEndpoint("/ws").setAllowedOriginPatterns("*").withSockJS();
+//        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 
     // 메시지 브로커 구성 (메시지를 브로드캐스팅)
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/socket");
+//        registry.enableSimpleBroker("/socket");
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/queue/", "/topic/", "/user/");   // Enables a simple in-memory broker
+        registry.setUserDestinationPrefix("/user");
     }
 
 
