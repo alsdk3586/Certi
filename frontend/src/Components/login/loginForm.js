c665d57d408c955d968a73ba05824480e6353b26
JeeyoungKim <jiiioiyoung96@gmail.com>
2021-05-03 16:12:09 +0900

    feat: 🍎 로그인 디자인중

---
+++ frontend/src/Components/login/loginForm.js
@@ -0,0 +1,35 @@
+import React, {Component} from 'react';
+
+class LoginForm extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: "",
+            password: ""
+        };
+
+        this.handleInputChange = this.handleInputChange.bind(this);
+    };
+    handleInputChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+    handleOnClick = (e) => {
+        console.log(this.state.email, this.state.password);
+    }
+    render() {
+        return (
+            <div class="loginForm">
+                <div class="input">
+                    <input type="email" id="inputEmail" placeholder="이메일" name="email" onChange={this.handleChange} />
+                    <br></br>
+                    <input type="password" id="inputPassword" placeholder="비밀번호" name="password" onChange={this.handleChange} />
+                </div>
+                <button class="loginButton"
+                    type="button"
+                    onClick={this.handleOnClick}>
+                    로그인
+                </button>
+            </div>
+        );
+    };
+} export default LoginForm;
--- frontend/src/Routes/Login.js
+++ frontend/src/Routes/Login.js
@@ -1,31 +1,35 @@
 import React, {Component} from 'react';
 import '../Components/css/login/style.scss';
+import { BigLogo } from '../assets/index'
+import LoginForm from '../Components/login/loginForm'
 
 class Login extends Component{
-    constructor(props) {
-        super(props);
-        this.state = { email: "", phone: "" };
-    };
-    handleChange = (e) => {
-        this.setState({ [e.target.name]: e.target.value });
-    };
-    handleOnClick = (e) => {
-        console.log(this.state.email, this.state.phone);
-    }
     render() {
         return (
-            <form className="login">
-                <div class="logo"> 자격증닷컴</div>  
-                <div class="contents">
-                    <input type="email" id="inputEmail" placeholder="이메일" name="email" onChange={this.handleChange} />
-                    <br></br>
-                    <input type="password" id="inputPassword" placeholder="비밀번호" name="password" onChange={this.handleChange} />
-                </div>
-                <button class="loginButton"
-                    type="button"
-                    onClick={this.handleOnClick}>
-                    로그인
-                </button>
-            </form>);
+            <div class="login">
+                <div class="logo">
+                    <BigLogo/>
+                </div>  
+                
+                <LoginForm/>
+            </div>
+        );
     };
 } export default Login;
+
+
+// import React from 'react';
+// import '../Components/css/login/style.scss';
+
+// const Login = ({ children }) => (
+//     <div class='login'>
+//         <div class='logoWrapper'>
+//             <div class='logo' onclick="location.href='/'">자격증닷컴</div>
+//         </div>
+//         <contents>
+//             {children}
+//         </contents>
+//     </div>
+// );
+
+// export default Login;
