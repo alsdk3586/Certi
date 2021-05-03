4af539e9708f38f596984dc06a185f8806aa730d
JeeyoungKim <jiiioiyoung96@gmail.com>
2021-05-03 13:32:56 +0900

    feat: 🍎 로그인 마크업(디자인x)

--- frontend/src/Routes/Login.js
+++ frontend/src/Routes/Login.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import '../../Components/css/login/style.scss';
+import React, {Component} from 'react';
+import '../Components/css/login/style.scss';
 
-const Login = ({children}) => (
-    <login>
-        <logoWrapper>
-            <logo to="/">자격증닷컴</logo>
-        </logoWrapper>
-        <contents>
-            {children}
-        </contents>
-    </login>
-);
-
-export default Login;
+class Login extends Component{
+    constructor(props) {
+        super(props);
+        this.state = { email: "", phone: "" };
+    };
+    handleChange = (e) => {
+        this.setState({ [e.target.name]: e.target.value });
+    };
+    handleOnClick = (e) => {
+        console.log(this.state.email, this.state.phone);
+    }
+    render() {
+        return (
+            <form className="login">
+                <div class="logo"> 자격증닷컴</div>  
+                <div class="contents">
+                    <input type="email" id="inputEmail" placeholder="이메일" name="email" onChange={this.handleChange} />
+                    <br></br>
+                    <input type="password" id="inputPassword" placeholder="비밀번호" name="password" onChange={this.handleChange} />
+                </div>
+                <button class="loginButton"
+                    type="button"
+                    onClick={this.handleOnClick}>
+                    로그인
+                </button>
+            </form>);
+    };
+} export default Login;
