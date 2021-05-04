a34d15f40c18a19f956d60b84706e9f06d767710
JeeyoungKim <jiiioiyoung96@gmail.com>
2021-05-04 16:24:54 +0900

    feat: 🍎 login input form design

--- frontend/src/Components/App.js
+++ frontend/src/Components/App.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import Router from './Router';
 import GlobalStyles from './GlobalStyles';
-import { Provider } from 'react-redux';
 
-const App = ({store}) => {
+function App() {
   return (
-    <Provider store={store}>
+    <>
       <Router />
       <GlobalStyles />
-    </Provider>
+    </>
   );
 }
+
 export default App;
+
+// import React from 'react';
+// import Router from './Router';
+// import GlobalStyles from './GlobalStyles';
+// import { Provider } from 'react-redux';
+
+// const App = ({store}) => {
+//   return (
+//     <Provider store={store}>
+//       <Router />
+//       <GlobalStyles />
+//     </Provider>
+//   );
+// }
+// export default App;
--- frontend/src/Components/login/loginForm.js
+++ frontend/src/Components/login/loginForm.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as authActions from '../../redux/modules/auth';
+// import { connect } from 'react-redux';
+// import { bindActionCreators } from 'redux';
+// import * as authActions from '../../redux/modules/auth';
 
 import InputForm from './inputForm';
 import LoginButton from './loginButton';
@@ -9,51 +9,31 @@ import RegisterButton from './registserButton';
 import '../css/login/style.scss';
 
 class LoginForm extends Component {
-    handleChange = (e) => {
-        const { AuthActions } = this.props;
-        const { name, value } = e.target;
-
-        AuthActions.changeInput({
-            name,
-            value,
-            form: 'login'
-        });
-    }
-
-    componentWillUnmount() {
-        const { AuthActions } = this.props;
-        AuthActions.initializeForm('login')
-    }
-
     render() {
-        const { email, password } = this.props.form.toJS(); // form 에서 email 과 password 값을 읽어옴
-        const { handleChange } = this;
+        // const { email, password } = this.props.form.toJS(); // form 에서 email 과 password 값을 읽어옴
+        // const { handleChange } = this;
 
         return (
             <div class="loginForm">
                 <InputForm
                     label="이메일"
-                    name="email"
-                    value={email} 
-                    onChange={handleChange}/>
+                    name="email"/>
                 <InputForm
                     label="비밀번호"
                     name="password"
-                    type="password"
-                    value={password} 
-                    onChange={handleChange}/>
+                    type="password"/>
                 <LoginButton>로그인</LoginButton>
                 <RegisterButton to="/register">회원가입</RegisterButton>
             </div>
         );
     }
 }
-
-export default connect(
-    (state) => ({
-        form: state.auth.getIn(['login', 'form'])
-    }),
-    (dispatch) => ({
-        AuthActions: bindActionCreators(authActions, dispatch)
-    })
-)(LoginForm);
+export default LoginForm;
+// export default connect(
+//     (state) => ({
+//         form: state.auth.getIn(['login', 'form'])
+//     }),
+//     (dispatch) => ({
+//         AuthActions: bindActionCreators(authActions, dispatch)
+//     })
+// )(LoginForm);
