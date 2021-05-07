6939de20665e6581cced6dcbfc8ba024a9b8af71
JeeyoungKim <jiiioiyoung96@gmail.com>
2021-05-07 11:07:18 +0900

    feat: 🍎 이메일&비밀번호 유효성 검사 후 로그인 버튼 활성화

--- frontend/src/Components/login/loginForm.js
+++ frontend/src/Components/login/loginForm.js
@@ -12,10 +12,13 @@ class LoginForm extends Component {
             userPassword: '',
             token: localStorage.getItem("token") || '',
             hasLoginFailed: false,
-            showSuccessMessage: false
+            showSuccessMessage: false, // 로그인 성공 시 버튼 활성화
+            buttonColor: "background: rgba(238, 238, 238, 0.3)",
+            textColor: "color: #FFFFFF"
         }
         this.handleChange = this.handleChange.bind(this)
         this.loginClicked = this.loginClicked.bind(this)
+        this.handleActivation = this.handleActivation.bind(this)
     }
 
     handleChange(event) {
@@ -37,32 +40,62 @@ class LoginForm extends Component {
             });
             LoginService.registerSuccessfulLoginForJwt(this.state.userEmail, this.state.token)
             
-            // 로그인 성공 시 페이지 이동
-            this.props.history.push(`/user`)
-            // this.props.push('/')
+            if (this.state.showSuccessMessage === true) { // 로그인 성공 시 페이지 이동
+                this.props.history.push(`/user`)
+                // this.props.push('/')
+            }
+            else {
+                alert("로그인에 실패하였습니다")
+            }
+            
         }).catch( () =>{
             this.setState({showSuccessMessage:false})
             this.setState({hasLoginFailed:true})
         })
     }
+
+    handleActivation() {
+        if (this.state.userEmail.includes("@") && this.state.userPassword.length >= 3) {
+            this.setState({
+                buttonColor: "#e1eef6",
+                textColor: "#000000",
+                showSuccessMessage: true
+            })
+        }
+        else {
+            this.setState({
+                buttonColor: "rgba(238, 238, 238, 0.3)",
+                showSuccessMessage: false
+            })
+        }
+    }
     
     render() {
         return (
-            <div class="loginForm">
+            <div class="loginForm" onKeyUp={this.handleActivation}>
                 <InputForm
                     label="이메일"
                     name="userEmail"
+                    required
                     value={this.state.userEmail}
                     onChange={this.handleChange}
                 />
                 <InputForm
                     label="비밀번호"
                     name="userPassword"
+                    required
                     type="password"
                     value={this.state.userPassword}
                     onChange={this.handleChange}
                 />
-                <div class="loginButton" onClick={this.loginClicked}>로그인</div>
+                <div
+                    class="loginButton"
+                    onClick={this.loginClicked}
+                    style={{
+                        backgroundColor: this.state.buttonColor,
+                        color: this.state.textColor
+                    }}>
+                    로그인</div>
             </div>
         );
     }
--- frontend/src/Components/register/registerForm.js
+++ frontend/src/Components/register/registerForm.js
@@ -44,7 +44,7 @@ class RegisterForm extends Component {
     duplicateCheckClick() {
         RegisterService
             .duplicateCheckClick(this.state.userNickname)
-            .then((response) => {
+            .then(() => {
                 alert("사용가능한 닉네임입니다");
 
             }).catch((response) => {
