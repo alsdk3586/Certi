5ddf9cd8863787c162eea257fce25efebba348ad
김혜민 <alsdk3586@hanmail.net>
2021-05-11 15:12:43 +0900

    feat: add board detail page

--- frontend/src/Components/DetailBoard.js
+++ frontend/src/Components/DetailBoard.js
@@ -1,15 +1,43 @@
 import React, { useState, useEffect } from "react";
 import "../Components/css/css.scss";
 import { boardApi } from "../utils/axios";
-
-function DetailTitle({ data }) {
+import { FiEye, FiCalendar } from "react-icons/fi";
+import { BiDislike, BiLike } from "react-icons/bi";
+function Detail({ data }) {
   console.log(data);
   return (
-    <div>
-      <div id="detailBoardTitle">{data.boardTitle}</div>
-      <div>{data.boardCreate}</div>
-      <div>{data.boardHit}</div>
-      <div>{data.boardWriter}</div>
+    <div style={{ flex: 1 }}>
+      <hr style={{ height: 3 }}></hr>
+      <div id="detailBoardTop">
+        <div style={{ flex: 8.5, float: "left", paddingLeft: "10px" }}>
+          <div>
+            <h1 id="detailBoardTitle">{data.boardTitle}</h1>
+            <div>{data.boardWriter}</div>
+          </div>
+        </div>
+        <div style={{ flex: 1.5 }}>
+          <div>
+            <div>
+              <FiCalendar />
+              &nbsp;&nbsp;&nbsp;
+              {data.boardCreate}
+            </div>
+            <br></br>
+            <div>
+              <FiEye />
+              &nbsp;&nbsp;&nbsp;
+              {data.boardHit}
+            </div>
+          </div>
+        </div>
+      </div>
+      <hr></hr>
+      <div>
+        <div id="detailBoardContent">{data.boardContent}</div>
+        {/* <BiDislike />
+        <BiLike/> */}
+      </div>
+      <div>{/* 자신의 글이면 삭제하기와 수정하기 활성화하기 */}</div>
     </div>
   );
 }
@@ -20,6 +48,7 @@ export default function DetailBoard({ match }) {
 
   const fill = async () => {
     const res = await boardApi.getDetailBoard(no);
+    console.log(res);
     setData(res);
   };
 
@@ -29,7 +58,7 @@ export default function DetailBoard({ match }) {
 
   return (
     <div id="detailBoard">
-      <DetailTitle data={data} />
+      <Detail data={data} />
     </div>
   );
 }
