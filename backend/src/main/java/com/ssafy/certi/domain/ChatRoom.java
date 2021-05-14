a2d92848b053092238e7970dcba9d96c602608f2
multicampus <brotherspear106@gmail.com>
2021-05-14 11:48:52 +0900

    docs: @Prepersist annotation
    docs: @Prepersist annotation

--- backend/src/main/java/com/ssafy/certi/domain/ChatRoom.java
+++ backend/src/main/java/com/ssafy/certi/domain/ChatRoom.java
@@ -5,8 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
@@ -14,19 +13,29 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatRoom {
 
-    @Id
-    private String certificateCode;
-    private LocalDateTime localDateTime;
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "chatroom_id")
+    private int chatroomId;
 
+    @ManyToOne
+    @JoinColumn(name = "certificate_code")
+    private Certificate certificateCode;
+
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    public void createdAt() {
+        this.createdAt = LocalDateTime.now();
+    }
 
     //채팅방 생성
-    @Builder
-    public static ChatRoom createRoom(String certificateCode){
-        ChatRoom chatRoom = new ChatRoom();
-        chatRoom.certificateCode = certificateCode;
-        chatRoom.localDateTime = LocalDateTime.now();
-
-        return chatRoom;
-    }
+//    @Builder
+//    public static ChatRoom createRoom(String certificateCode){
+//        ChatRoom chatRoom = new ChatRoom();
+//        chatRoom.certificateCode = certificateCode;
+//        chatRoom.localDateTime = LocalDateTime.now();
+//
+//        return chatRoom;
+//    }
 
 }
